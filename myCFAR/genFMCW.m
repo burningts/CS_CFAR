c3e56f3f5bd357b8d5d20d151495e899a7884d13
burning <burningf@qq.com>
2022-01-18 18:51:00 +0800

    可在fmcw稀疏信号中找到目标

---
+++ myCFAR/genFMCW.m
@@ -0,0 +1,41 @@
+%%
+function u = genFMCW(n,r,signal_SNR)
+    %工作信号参数
+    f0   = 77e9;                                %工作频率，单位：Hz
+    fsamp= 20e6;                               %采样频率，单位：Hz
+    ts = 1/fsamp; 
+    ti = (0:n-1)*ts; 
+    band = 3.6e9;                                %扫频带宽,单位：Hz     
+    tp = 60e-6;                              %扫频周期,单位：s  
+    u = band/tp; 
+    tao = 2.*r./3e8;                             %时延
+    freq_gap = u * tao;  %频差
+
+    % 产生信号
+    xs = zeros(length(r),n);
+    s = zeros(1,n);  %生成的中频IF信号s
+    for i = 1:length(r)
+        xs(i,:) = exp(1j*2*pi*(u*tao(i)*ti - 0.5*u*tao(i)^2 + f0*tao(i)));
+        s = s + xs(i,:);
+    end
+    s = awgn(s,signal_SNR);
+    fsampu = (0:n-1)*(fsamp/n);
+    r = fsampu*3e8/(2*u);
+    u = fft(s);
+    u1 = abs(u);
+
+%     figure, 
+%     subplot(1,2,1);
+%     plot(real(s));
+%     grid on;
+%     title('中频IF信号时域波形');
+%     subplot(1,2,2);
+%     plot(r,20*log10(u1/max(u1)));       %横轴以MHz为单位，纵轴是dB形式
+%     xlim([0,25]);
+%     ylim([-100,10]);
+%     title('中频IF信号频谱');
+%     xlabel('Range(m)');
+%     ylabel('Magnitude(dB)');
+%     grid on;     
+end
+
---
+++ myCFAR/main.m
@@ -0,0 +1,83 @@
+%% 压缩感知重构算法测试
+clear; clc;
+m = 256;%观测值个数
+n = 1024;%信号x的长度
+range = [2,5,6];                                     %目标距离,单位：m
+signal_SNR = 20;                            %信号信噪比,单位：dB
+
+x = genFMCW(n,range,signal_SNR);
+x = x(:);
+Psi = eye(n);%x本身是稀疏的，定义稀疏矩阵为单位阵theta=Psi*x
+Phi = randn(m,n)/sqrt(m);%测量矩阵为高斯矩阵
+A = Phi * Psi;%传感矩阵
+y = Phi * x;%得到观测向量y
+
+%% 恢复重构信号x
+% theta = CS_SAMP( y,A,5);
+S = 4;
+ [y_rows,y_columns] = size(y);
+    if y_rows<y_columns
+        y = y';%y should be a column vector
+    end
+    [m,n] = size(A);%传感矩阵A为M*N矩阵
+    theta = zeros(n,1);%用来存储恢复的theta(列向量)
+    Pos_theta = [];%用来迭代过程中存储A被选择的列序号
+    Xmax = [];
+    r_n = y;%初始化残差(residual)为y
+    L = S;%初始化步长(Size of the finalist in the first stage)
+    Stage = 1;%初始化Stage
+    IterMax = 5;
+    for ii=1:IterMax%最多迭代M次
+        %(1)Preliminary Test
+        product = A'*r_n;%传感矩阵A各列与残差的内积
+        [val,pos]=sort(abs(product),'descend');%降序排列
+        Sk = pos(1:L);%选出最大的L个
+        Xmaxi = x(Sk);
+        Xmax = union(Xmax,Xmaxi);
+        %(2)Make Candidate List
+        Ck = union(Pos_theta,Sk);
+        %(3)Final Test
+        if length(Ck)<=m
+            At = A(:,Ck);%将A的这几列组成矩阵At
+        else
+            theta_ls=0;
+            break;
+        end
+        %y=At*theta，以下求theta的最小二乘解(Least Square)
+        theta_ls = (At'*At)^(-1)*At'*y;%最小二乘解
+        [val,pos]=sort(abs(theta_ls),'descend');%降序排列
+        F = Ck(pos(1:L));
+        %(4)Compute Residue
+        %A(:,F)*theta_ls是y在A(:,F)列空间上的正交投影
+        theta_ls1 = (A(:,F)'*A(:,F))^(-1)*A(:,F)'*y;
+        r_new = y - A(:,F)*theta_ls1;%更新残差r
+        norm_r = norm(r_n);
+        norm_r_new = norm(r_new);
+        if norm(r_new)<1e-6%halting condition true 
+            Pos_theta = F;
+            %r_n = r_new;%更新r_n以便输出最新的r_n
+            break;%quit the iteration
+        elseif norm(r_new)>=norm(r_n)%stage switching 
+            Stage = Stage + 1;%Update the stage index 
+            L = Stage*S;%Update the size of finalist
+            if ii == IterMax%最后一次循环
+                Pos_theta = F;%更新Pos_theta以与theta_ls匹配，防止报错
+            end
+            %ii = ii - 1;%迭代次数不更新
+        else
+            Pos_theta = F;%Update the finalist Fk
+            r_n = r_new;%Update the residue
+        end
+    end
+    theta(Pos_theta)=theta_ls1;%恢复出的theta
+
+
+%% 绘图
+figure;
+% plot(x_r,'k.-');%绘出x的恢复信号
+% hold on;
+plot(real(x),'r');%绘出原信号x
+% hold off;
+% legend('Recovery','Original')
+% fprintf('\n恢复残差：');
+% norm(x_r-x)%恢复残差
